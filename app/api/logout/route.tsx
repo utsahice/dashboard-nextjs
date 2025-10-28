3507715267e116b67ae9148a481e83b795ef6793
utsahice <utsahic.encircle@gmail.com>
2025-10-28 18:49:54 +0530

    fix : logout error in vercel

--- app/api/logout/route.tsx
+++ app/api/logout/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function POST() {
   const res = NextResponse.redirect(
-    new URL('/login', process.env.NEXT_PUBLIC_URL ),
+    new URL('/login', process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'),
   )
   res.cookies.set('user_email', '', { maxAge: 0, path: '/' }) // delete cookie
   return res
