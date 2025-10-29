c7d1fa96400f637a74c7c4b5c9654c44e529aca1
utsahice <utsahic.encircle@gmail.com>
2025-10-29 10:52:54 +0530

    fix : not goes to home page on vercel

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const userEmail = req.cookies.get('user_email')?.value;
   if (userEmail) {
-    if (pathname === '/login' || pathname === '/signup' || pathname === '/') {
+    if (pathname === '/login' || pathname === '/signup') {
       return NextResponse.redirect(new URL('/dashboard', req.url));
     }
     return NextResponse.next();
   }
-  if (pathname === '/dashboard' || pathname === '/') {
+  if (pathname === '/dashboard') {
     return NextResponse.redirect(new URL('/login', req.url));
   }
   return NextResponse.next();
