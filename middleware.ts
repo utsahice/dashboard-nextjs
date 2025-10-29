9d85b70e1c7216d0342775f14088cd13dbd8b645
utsahice <utsahic.encircle@gmail.com>
2025-10-29 11:04:42 +0530

    fix : dashboard not showing issue on vercel

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,7 @@ export function middleware(req: NextRequest) {
   if (pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
-.
+  
   return NextResponse.next();
 }
 
